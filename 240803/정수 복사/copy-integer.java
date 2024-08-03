282723906a9c1372b564cb6e4f9c19d9e31ed9cf
Yoonho Cho <124619604+dev-yoonho@users.noreply.github.com>
2024-08-03 23:34:16 +0900

    Title: 정수 복사, Time: 93ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240803/정수 복사/copy-integer.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        b = a;
+
+        System.out.printf("%d %d\n", a, b);
+        System.out.println(a * b);
+    }
+}
